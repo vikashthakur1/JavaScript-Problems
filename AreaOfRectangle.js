2a4275215b237a8d5edea867801cc73102beb9c5
Vikash <vikastsr@gmail.com>
2025-04-18 13:10:34 +0530

    2nd Changes

---
+++ AreaOfRectangle.js
@@ -0,0 +1,6 @@
+function AreaOfRectangle(l,w) {
+    const area=l*w
+    return " Area of Rectangle "+area 
+}
+
+console.log(AreaOfRectangle(100,200))
--- PrintTheSumOfTwoNumber.js
+++ PrintTheSumOfTwoNumber.js
@@ -4,5 +4,5 @@ function sum(a,b)
  return sum;
 }
 
-let sum1 = sum(5,4)
+let sum1 = sum(124,4)
 console.log('Sum of two number is ',sum1)
